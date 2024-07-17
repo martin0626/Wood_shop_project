e6d0a30f77542d3c59288c30b4e145ad08d8c9eb
martin0626 <scbsteel12@gmail.com>
2024-07-17 18:54:15 +0300

    Added Top 10 products slider

--- wood_store_frontend/src/components/Home/TopProducts.jsx
+++ wood_store_frontend/src/components/Home/TopProducts.jsx
@@ -1,9 +1,38 @@
+import Slider from "react-slick";
+import classes from "./TopProducts.module.css"
+import SingleProductComponent from "../Products/SingleProduct";
+
 export default function TopProducts({products}){
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
+
     return (
-        <section>
-            {products.map(pr=>
-                <h1>{pr.name}</h1>
-            )}
+        <section className={classes.topSection}>
+                <div className={classes.topHeader}>
+                    <h1>Check Our Top 10 Products</h1>
+
+                </div>
+                <div className={classes['product-slider-container']}>
+                    <Slider  {...settings} className="image-slider">
+                            {products.map((product) => (
+                                
+                                <div className={classes.cardTop} key={product.id}>
+                                    <div className={classes.imageCard}>
+                                        <img src='https://i.etsystatic.com/15705690/r/il/33dc60/1843727366/il_fullxfull.1843727366_flps.jpg' alt="product IMG" />
+                                    </div>
+                                    <div className={classes.descriptionTopPr}>
+                                        <p>{product.name}</p>
+                                        <p>{product.price}BGN</p>
+                                    </div>
+                                </div>
+                            ))}
+                    </Slider>
+                </div>
         </section>
     )
 }
